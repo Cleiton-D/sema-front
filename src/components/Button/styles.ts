ebbaca00fea05f1905677a9f2198c9fab822f2ed
thiagopersch <thiagopersch@gmail.com>
2021-04-10 12:53:46 -0400

    Created button component

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,86 @@
+import styled, { css, DefaultTheme } from 'styled-components';
+import { darken } from 'polished';
+
+import { ButtonProps } from './';
+
+export type WrapperProps = {
+  hasIcon: boolean;
+} & Pick<ButtonProps, 'size' | 'fullWidth' | 'styleType'>;
+
+const wrapperModifiers = {
+  small: (theme: DefaultTheme) => css`
+    height: 3rem;
+    font-size: ${theme.font.sizes.xsmall};
+  `,
+  medium: (theme: DefaultTheme) => css`
+    height: 4rem;
+    font-size: ${theme.font.sizes.small};
+    padding: ${theme.spacings.xxsmall} ${theme.spacings.medium};
+  `,
+  large: (theme: DefaultTheme) => css`
+    height: 5rem;
+    font-size: ${theme.font.sizes.medium};
+    padding: ${theme.spacings.xxsmall} ${theme.spacings.xlarge};
+  `,
+  fullWidth: () => css`
+    width: 100%;
+  `,
+  withIcon: (theme: DefaultTheme) => css`
+    svg {
+      width: 1.5rem;
+      & + span {
+        margin-left: ${theme.spacings.xxsmall};
+      }
+    }
+  `,
+  disabled: () => css`
+    &:disabled {
+      cursor: not-allowed;
+      filter: saturate(30%);
+    }
+  `,
+  normal: () => css``,
+  rounded: (theme: DefaultTheme) => css`
+    border-radius: ${theme.border.rounded};
+  `,
+  outlined: (theme: DefaultTheme) => css`
+    background: ${theme.background.transparent};
+    box-sizing: border-box;
+    border: 0.2rem solid ${theme.background.primary};
+    color: ${theme.colors.primary};
+    font-weight: ${theme.font.bold};
+    border-radius: ${theme.border.rounded};
+
+    &:hover {
+      background: ${theme.background.transparent};
+    }
+  `
+};
+
+export const Wrapper = styled.button<WrapperProps>`
+  ${({ theme, size, fullWidth, hasIcon, disabled, styleType }) => css`
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    background: ${theme.background.primary};
+    color: ${theme.colors.white};
+    border: 0;
+    outline: none;
+    border-radius: 0.6rem;
+    padding: ${theme.spacings.xxsmall};
+    text-decoration: none;
+    cursor: pointer;
+    font-weight: ${theme.font.bold};
+    transition: background 0.2s ease;
+
+    &:hover {
+      background: ${darken(0.1, theme.background.primary)};
+    }
+
+    ${!!styleType && wrapperModifiers[styleType](theme)}
+    ${!!fullWidth && wrapperModifiers.fullWidth()}
+    ${!!size && wrapperModifiers[size](theme)}
+      ${!!hasIcon && wrapperModifiers.withIcon(theme)}
+      ${disabled && wrapperModifiers.disabled()};
+  `}
+`;
