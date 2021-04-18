45c2871d4c53263cd6932fe900ab7e5800b52f5b
thiagopersch <thiagopersch@gmail.com>
2021-04-18 17:18:53 -0400

    Create config to SVG component imports

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,6 +4,14 @@ module.exports = {
   webpackFinal: async (config) => {
     config.resolve.modules.push(`${process.cwd()}/src`);
 
+    const assetRule = config.module.rules.find(({ test }) => test.test('.svg'));
+    assetRule.exclude = /\.svg$/;
+
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack']
+    });
+
     return config;
   }
 };
