66dfce8328245fa09af0266c3ea8e74d394207a2
Cleiton Dione Ahnerth Kiper <cleitonahnerth@gmail.com>
2021-04-18 20:20:43 -0400

    set application background to storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,6 +3,18 @@ import { ThemeProvider } from 'styled-components';
 import theme from 'styles/theme';
 import GlobalStyles from 'styles/global';
 
+export const parameters = {
+  backgrounds: {
+    default: 'sema-light',
+    values: [
+      {
+        name: 'sema-light',
+        value: theme.colors.mainBg
+      }
+    ]
+  }
+};
+
 export const decorators = [
   (Story) => (
     <ThemeProvider theme={theme}>
