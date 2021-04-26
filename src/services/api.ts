d661236bc6b7e23b63e1850a159d4fd6abfef414
Cleiton Dione Ahnerth Kiper <cleitonahnerth@gmail.com>
2021-04-25 20:17:15 -0400

    create swr fetcher

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,7 @@
 import { useMemo } from 'react';
 import { Session } from 'next-auth';
-import axios from 'axios';
+import useSWR, { SWRConfiguration } from 'swr';
+import axios, { AxiosResponse } from 'axios';
 
 const createApi = (session?: Session | null) => {
   const jwt = session?.jwt;
@@ -22,4 +23,44 @@ export function initializeApi(session?: Session | null) {
 export function useApi(session?: Session | null) {
   const store = useMemo(() => createApi(session), [session]);
   return store;
+}
+
+type FetchOptions<T> = {
+  session?: Session | null;
+  formatter?: (value: any) => T;
+};
+
+export function useSWRFetch<T = unknown>(
+  url: string,
+  options: FetchOptions<T> = {},
+  swrConfig: SWRConfiguration = {}
+) {
+  const api = useApi(options.session);
+  const newSwrConfig = useMemo(() => {
+    const initialData = swrConfig.initialData;
+    if (initialData) {
+      const newInitalData: Partial<AxiosResponse<unknown>> = {
+        data: initialData
+      };
+      return { ...swrConfig, initialData: newInitalData };
+    }
+
+    return swrConfig;
+  }, [swrConfig]);
+
+  const { data, ...rest } = useSWR<AxiosResponse<unknown>>(
+    url,
+    api.get,
+    newSwrConfig
+  );
+
+  return useMemo(() => {
+    const responseData = data?.data as T | undefined;
+
+    if (!responseData || !options.formatter)
+      return { data: responseData, ...rest };
+
+    const newData = options.formatter(responseData);
+    return { data: newData, ...rest };
+  }, [options, data, rest]);
 }
