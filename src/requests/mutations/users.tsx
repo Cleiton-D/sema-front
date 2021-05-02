d439eeca208ae9a59b93cf9ef03be44fd3776578
Tiago Persch <tiagopersch@rubeus.com.br>
2021-05-01 22:01:41 -0400

    Created mutation to delete user

--- src/requests/mutations/users.tsx
+++ src/requests/mutations/users.tsx
@@ -5,6 +5,7 @@ import { v4 as uuidv4 } from 'uuid';
 import { ModalRef } from 'components/Modal';
 
 import { initializeApi, useMutation } from 'services/api';
+import { FormattedUser } from 'models/User';
 
 import ToastContent from 'components/ToastContent';
 
@@ -38,5 +39,36 @@ export function useAddUserMutation(
     renderError: (newUser) => `Falha ao inserir usuário ${newUser.username}`,
     renderSuccess: (newUser) =>
       `Usuário ${newUser.username} inserido com sucesso`
+  });
+}
+
+export function useDeleteUserMutation(session?: Session | null) {
+  const deleteUser = useCallback(
+    async (user: FormattedUser) => {
+      const api = initializeApi(session);
+
+      return api.delete(`/users/${user.id}`);
+    },
+    [session]
+  );
+
+  return useMutation('delete-user', deleteUser, {
+    linkedQueries: {
+      'get-users': (old, deletedUser) =>
+        old.map((user: FormattedUser) =>
+          user.id === deletedUser.id ? { ...user, disabled: true } : user
+        )
+    },
+    renderLoading: function render(deletedUser) {
+      return (
+        <ToastContent showSpinner>
+          Removendo usuário {deletedUser.username}...
+        </ToastContent>
+      );
+    },
+    renderError: (deletedUser) =>
+      `Falha ao remover usuário ${deletedUser.username}`,
+    renderSuccess: (deletedUser) =>
+      `Usuário ${deletedUser.username} removido com sucesso`
   });
 }
