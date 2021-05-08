bef0d1c35626fec83e14931330bf95631b1125a6
Cleiton Dione Ahnerth Kiper <cleitonahnerth@gmail.com>
2021-05-08 14:10:29 -0400

    add enrolls count column

--- src/requests/queries/schools.ts
+++ src/requests/queries/schools.ts
@@ -1,13 +1,15 @@
 import { Session } from 'next-auth';
 import { useQuery } from 'react-query';
 
-import { School } from 'models/School';
+import { SchoolWithEnrollCount } from 'models/School';
 import { initializeApi } from 'services/api';
 
 export const listSchools = (session?: Session | null) => {
   const api = initializeApi(session);
 
-  return api.get<School[]>('/schools').then((response) => response.data);
+  return api
+    .get<SchoolWithEnrollCount[]>('/schools')
+    .then((response) => response.data);
 };
 
 export const useListSchools = (session?: Session | null) => {
