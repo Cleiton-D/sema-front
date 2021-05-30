bda841f5f9a3bf45f6cd0a96a184fa5906c11e52
Cleiton Dione Ahnerth Kiper <cleitonahnerth@gmail.com>
2021-05-30 10:33:47 -0400

    fix _app page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,53 +1,46 @@
-import { useMemo } from 'react';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import NextNprogress from 'nextjs-progressbar';
 import { ToastContainer } from 'react-toastify';
-import { Provider as JotaiProvider } from 'jotai';
 
 import 'react-toastify/dist/ReactToastify.css';
+import 'react-day-picker/lib/style.css';
 
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { QueryClientProvider } from 'react-query';
 import { Hydrate } from 'react-query/hydration';
 import { Provider as AuthProvider } from 'next-auth/client';
 import { ThemeProvider } from 'styled-components';
 
+import { AtomProvider, AtomHydrator } from 'hooks/AtomProvider';
+
 import GlobalStyles from 'styles/global';
 import theme from 'styles/theme';
 
-const App = ({ Component, pageProps }: AppProps) => {
-  const queryClient = useMemo(
-    () =>
-      new QueryClient({
-        defaultOptions: {
-          queries: {
-            staleTime: 10000
-          }
-        }
-      }),
-    []
-  );
+import { queryClient } from 'services/api';
 
+const App = ({ Component, pageProps }: AppProps) => {
   return (
     <AuthProvider session={pageProps.session}>
       <QueryClientProvider client={queryClient}>
         <Hydrate state={pageProps.dehydratedState}>
-          <JotaiProvider>
-            <ThemeProvider theme={theme}>
-              <Head>
-                <title>SEMA</title>
-              </Head>
-              <GlobalStyles />
-              <NextNprogress
-                color={theme.colors.primary}
-                startPosition={0.3}
-                stopDelayMs={200}
-                height={5}
-              />
-              <Component {...pageProps} />
-              <ToastContainer />
-            </ThemeProvider>
-          </JotaiProvider>
+          <AtomProvider initialState={pageProps.initialState}>
+            <AtomHydrator initialState={pageProps.initialState}>
+              <ThemeProvider theme={theme}>
+                <Head>
+                  <title>SEMA</title>
+                </Head>
+                <GlobalStyles />
+                <NextNprogress
+                  color={theme.colors.primary}
+                  startPosition={0.3}
+                  stopDelayMs={200}
+                  height={5}
+                />
+                <Component {...pageProps} />
+                <ToastContainer />
+              </ThemeProvider>
+            </AtomHydrator>
+          </AtomProvider>
         </Hydrate>
       </QueryClientProvider>
     </AuthProvider>
