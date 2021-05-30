075bd0a1af26e4eb30b162de6501a3389de6d2fa
Cleiton Dione Ahnerth Kiper <cleitonahnerth@gmail.com>
2021-05-30 10:43:46 -0400

    refactor class periods mapper to mask time

--- src/utils/mappers/classPeriodMapper.ts
+++ src/utils/mappers/classPeriodMapper.ts
@@ -1,14 +1,21 @@
 import { ClassPeriod, FormattedClassPeriod } from 'models/ClassPeriod';
+import { masks } from 'utils/masks';
 
-const translateDescription = (description: string) =>
+export const translateDescription = (description: string) =>
   ({
     MORNING: 'Matutino',
-    EVENING: 'Vespertino'
+    EVENING: 'Vespertino',
+    NOCTURNAL: 'Noturno'
   }[description] || description);
 
 export const classPeriodsMapper = (
   classPeriod: ClassPeriod
 ): FormattedClassPeriod => ({
   ...classPeriod,
-  translated_description: translateDescription(classPeriod.description)
+  translated_description: translateDescription(classPeriod.description),
+  time_start: masks.time(classPeriod.time_start),
+  time_end: masks.time(classPeriod.time_end),
+  class_time: masks.time(classPeriod.class_time),
+  break_time: masks.time(classPeriod.break_time),
+  break_time_start: masks.time(classPeriod.break_time_start)
 });
