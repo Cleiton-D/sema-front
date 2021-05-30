3b4bcce59ff22cff0718a7606de2758c107093ed
Cleiton Dione Ahnerth Kiper <cleitonahnerth@gmail.com>
2021-05-30 10:34:32 -0400

    refactor prefetch query to receive many items

--- src/utils/prefetch-query.ts
+++ src/utils/prefetch-query.ts
@@ -1,12 +1,21 @@
 import { QueryClient } from 'react-query';
 import { dehydrate } from 'react-query/hydration';
 
-export default async function prefetchQuery(
-  key: string,
-  fn: () => unknown | Promise<unknown>
-) {
+type Query = {
+  key: string;
+  fetcher: () => unknown | Promise<unknown>;
+};
+
+export default async function prefetchQuery(query: Query | Query[]) {
   const queryClient = new QueryClient();
-  await queryClient.fetchQuery(key, fn);
+  if (Array.isArray(query)) {
+    await Promise.all(
+      query.map(({ key, fetcher }) => queryClient.fetchQuery(key, fetcher))
+    );
+  } else {
+    const { key, fetcher } = query;
+    await queryClient.fetchQuery(key, fetcher);
+  }
 
   return dehydrate(queryClient);
 }
