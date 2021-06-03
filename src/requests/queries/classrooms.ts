8691c644024bac365e6df9e3d67a5ad5e3b15dfb
Cleiton Dione Ahnerth Kiper <cleitonahnerth@gmail.com>
2021-06-03 09:35:29 -0400

    create requests and mutations for classrooms

---
+++ src/requests/queries/classrooms.ts
@@ -0,0 +1,55 @@
+import { Session } from 'next-auth';
+import { useQuery } from 'react-query';
+import { v4 as uuidv4 } from 'uuid';
+
+import { Classroom } from 'models/Classroom';
+
+import { initializeApi } from 'services/api';
+
+export const addClassroomQueryMutation = (
+  old: Classroom[],
+  newItem: Classroom
+) => [...old, { ...newItem, uuid: uuidv4(), disabled: true }];
+
+export const deleteClassroomQueryMutation = (
+  old: Classroom[] | null,
+  removed: Classroom
+) => {
+  if (!old) return old;
+
+  return old.map((item) =>
+    item.id === removed.id ? { ...item, disabled: true } : item
+  );
+};
+
+type ListClassroomsFilters = {
+  school_id?: string;
+};
+
+export const listClassrooms = (
+  session: Session | null,
+  filters: ListClassroomsFilters = {}
+) => {
+  const api = initializeApi(session);
+
+  const { school_id } = filters;
+
+  return api
+    .get<Classroom[]>(`/schools/${school_id || 'me'}/classrooms`)
+    .then((response) => response.data);
+};
+
+export const useListClassrooms = (
+  session: Session | null,
+  filters: ListClassroomsFilters = {}
+) => {
+  const key = `list-classrooms-${JSON.stringify(filters)}`;
+  const result = useQuery(key, () => listClassrooms(session, filters));
+
+  return {
+    ...result,
+    key,
+    queryAddMutation: addClassroomQueryMutation,
+    queryRemoveMutation: deleteClassroomQueryMutation
+  };
+};
