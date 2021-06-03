6f52238496716d9cfe3b1d3b3f37616b24c32922
Cleiton Dione Ahnerth Kiper <cleitonahnerth@gmail.com>
2021-06-03 09:31:43 -0400

    improve Heading component

--- src/components/Heading/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled, { css } from 'styled-components';
-
-export const Wrapper = styled.h2`
-  ${({ theme }) => css`
-    color: ${theme.colors.secondary};
-    font-weight: ${theme.font.bold};
-    font-size: ${theme.font.sizes.xxlarge};
-  `}
-`;
